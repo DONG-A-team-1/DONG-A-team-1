ea3b2f8664223736ddcae030bd755b6cf3b1555e
Bengmii <a88738935@gmail.com>
2025-12-30 11:03:56 +0900

    local DB인 donga 로 연결해둠

--- util/database.py
+++ util/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
-url = 'mysql+pymysql://web_user:pass@localhost:3306/mydb'
+# url = 'mysql+pymysql://web_user:pass@localhost:3306/mydb'
+url = 'mysql+pymysql://web_user:pass@localhost:3306/donga'
 engine = create_engine(url, echo=False, pool_size=1)
 
 def get_engine():
