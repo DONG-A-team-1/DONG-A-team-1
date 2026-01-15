75a7d34997d4c7307e03dbb650c150ddcbf9a974
joejangwoo <nabbe123@naver.com>
2026-01-15 13:01:27 +0900

    나바꾼거 없는데?

--- api/user_embedding.py
+++ api/user_embedding.py
@@ -474,7 +474,7 @@ def recommend_articles(user_id: str, limit: int = 20,random: bool = False):
     if not random:
         return ranked[:limit]
     else:
-        return _soft_shuffle_topk(ranked, top_k=12, strength=1.2)[:limi
+        return _soft_shuffle_topk(ranked, top_k=12, strength=1.2)[:limit]
 
 
 if __name__ == "__main__":
